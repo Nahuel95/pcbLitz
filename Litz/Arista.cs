05a040be56d074add6d8b565b097b352626b8cc7
Nahuel95 <nahuel_f95@hotmail.es>
2020-03-03 15:57:10 -0300

    tipo coordenada, arista, programa base
    Se creo el progrma basico, el tipo arista y el tipo coordenada

---
+++ Litz/Arista.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Litz
+{
+    class Arista
+    {
+        private Coordenada inicio;
+        private Coordenada final;
+        private int numCanales;
+        private double[] offsetInicio;
+        private double[] offsetFinal;
+        private double longitud;
+        private int numTransposiciones;
+        private int angulo; //De 0 a 7, angulo * 45 = medida real.
+
+        public Arista(Coordenada inicio, Coordenada final, int numCanales, int anguloInicial, int anguloFinal){
+            this.inicio = inicio;
+            this.final = final;
+
+
+        }
+
+        
+    }
+    
+}
