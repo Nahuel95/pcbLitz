cbff3ed3761418a5ab2579774fe66899e34cc6d9
cbuzzio <cbuzzio@yahoo.com.ar>
2020-03-08 15:50:00 -0300

    Actualizacion Arista y Coordenada

--- Litz/Arista.cs
+++ Litz/Arista.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,15 @@ namespace Litz
         private double[] offsetInicio;
         private double[] offsetFinal;
         private double longitud;
-        private int numTransposiciones;
+        private int numTrans;
         private int angulo; //De 0 a 7, angulo * 45 = medida real.
 
-        public Arista(Coordenada inicio, Coordenada final, int numCanales, int anguloInicial, int anguloFinal){
+        public Arista(Coordenada inicio, Coordenada final, int numCanales){
             this.inicio = inicio;
             this.final = final;
             this.numCanales = numCanales;
+            this.longitud = Coordenada.distancia(inicio,final);
+            this.angulo = Math.floor(Math.atan2(final.y-inicio.y, final.x-inicio.x)/45.0);
 
 
         }
